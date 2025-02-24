0dd979a2a538435fcbfffc076bd971a2d8ddf29a
Tiffany Andriamiharimanana <tiffany.a@vffice.com>
2025-02-24 09:45:24 -0500

    added bg images

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i < numberOfDrumButtons; i++) { //all elements with class "drum"
-    document.querySelectorAll("button")[i].addEventListener("click", function () {
-        alert("I got clicked!"); //anonymous function
+for (var i = 0; i < numberOfDrumButtons; i++) {
+    
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+        var audio = new Audio('./sounds/tom-1.mp3');
+        audio.play();
+        
     });
 }
